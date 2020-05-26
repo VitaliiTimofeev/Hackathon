c67a91bb4fa639cfc4ecc1d55436cdd6f95bf73d
Vitaliy Timofeev <vital9xd@gmail.com>
2020-05-26 12:28:01 +0300

    ChangeGameOne

--- src/main/java/gameSelection/GameOne.java
+++ src/main/java/gameSelection/GameOne.java
@@ -11,48 +11,47 @@ public class GameOne {
 
     public static boolean playGameOne() {
 
-            int min = MIN;
-            int max = MAX;
-            Scanner sc = new Scanner(System.in);
-            Random rnd = new Random();
-            int resNum = rnd.nextInt(max - min) + min;
-            while (true) {
-                try {
-                    System.out.println("Для выхода введите \"выйти\"");
-                    System.out.println("если ваше число больше введите \"больше\", если меньше введите \"меньше");
-                    System.out.println("если число верно введи \"верно\".");
-                    System.out.println(resNum);
-                    String input = sc.nextLine();
-                    if(input.equalsIgnoreCase("выйти")){
-                        System.out.println("Спасибо за игру!");
-                        return true;
-                    }
-                    if (input.equalsIgnoreCase("меньше")) {
-                        max = resNum;
-                        resNum = rnd.nextInt(max - min) + min;
-                    } else if (input.equalsIgnoreCase("больше")) {
-                        min = resNum + 1;
-                        resNum = rnd.nextInt(max - min) + min;
-                    } else if (input.equalsIgnoreCase("верно")) {
-                        System.out.println("----------------------\n"+
-                                "Мне понравилось! Спасибо за игру\n"+
-                                        "----------------------\n"+
-                        "Давайте сыграем в Угадать число\n"+
-                                "----------------------\n");
-
-                        playGameTwo();
-                        
-                        return true;
-                    }
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Обманывать плохо, твоё число " + resNum);
+        int min = MIN;
+        int max = MAX;
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        int resNum = rnd.nextInt(max - min) + min;
+        while (true) {
+            try {
+                System.out.println("Для выхода введите \"выйти\"");
+                System.out.println("если ваше число больше введите \"больше\", если меньше введите \"меньше");
+                System.out.println("если число верно введи \"верно\".");
+                System.out.println(resNum);
+                String input = sc.nextLine();
+                if (input.equalsIgnoreCase("выйти")) {
+                    System.out.println("Спасибо за игру!");
                     return true;
                 }
+                if (input.equalsIgnoreCase("меньше")) {
+                    max = resNum;
+                    resNum = rnd.nextInt(max - min) + min;
+                } else if (input.equalsIgnoreCase("больше")) {
+                    min = resNum + 1;
+                    resNum = rnd.nextInt(max - min) + min;
+                } else if (input.equalsIgnoreCase("верно")) {
+                    System.out.println("----------------------\n" +
+                            "Мне понравилось! Спасибо за игру\n" +
+                            "----------------------\n" +
+                            "Давайте сыграем в Угадать число\n" +
+                            "----------------------\n");
+
+                    playGameTwo();
+
+                    return true;
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("Обманывать плохо, твоё число " + resNum);
+                return true;
             }
         }
-    public static boolean checkRiddle(String word) {
-        return word.equalsIgnoreCase("загадал");
     }
-    
-    
 }
+
+
+
+    
